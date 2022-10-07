5de24b0e0dd972067272c7ff1937710b27527705
F-balanta <jf.balanta@hotmail.com>
2022-10-06 20:57:25 -0500

    Decrypt Phrase

--- YeminusSoftware.Presentation/src/app/components/encryption/encryption.component.ts
+++ YeminusSoftware.Presentation/src/app/components/encryption/encryption.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../services/encryption/api.service'
 import {IPhrase} from '../../models/Interfaces/IPhrase'
 import {IDecryptDto} from "../../models/IPhraseDto";
+
 @Component({
   selector: 'app-encryption',
   templateUrl: './encryption.component.html',
@@ -9,55 +10,55 @@ import {IDecryptDto} from "../../models/IPhraseDto";
 })
 export class EncryptionComponent implements OnInit {
 
-  datatable:IPhrase[] = []
+  datatable: IPhrase[] = []
   dataTableDecrypt: IPhrase[] = [];
   phrase: IDecryptDto = {
-    code : 0
+    code: 0
   }
-  gettingPhrase:IPhrase = {
-    code : 0  ,
+  gettingPhrase: IPhrase = {
+    code: 0,
     phrase: '',
     key: 0
   }
 
   constructor(
-    private phraseService:ApiService
-  ) { }
+    private phraseService: ApiService
+  ) {
+  }
 
   ngOnInit(): void {
     this.onGetAllPhrases();
   }
 
-  onGetAllPhrases(){
-    this.phraseService.getAllPhrases().subscribe(data=>{
+  onGetAllPhrases() {
+    this.phraseService.getAllPhrases().subscribe(data => {
       this.datatable = data;
     })
   }
 
-
-  getDataEdit(select:IDecryptDto){
-     this.gettingPhrase.code = select.code
-      this.onDecryptPhrase(this.gettingPhrase)
+  getDataEdit(select: IDecryptDto) {
+    this.gettingPhrase.code = select.code
+    this.onDecryptPhrase(this.gettingPhrase)
   }
 
-  onDecryptPhrase(dato:IDecryptDto){
+  onDecryptPhrase(dato: IDecryptDto) {
     console.log(dato);
-    this.phraseService.decryptPhrase(dato).subscribe(data=>{
-
+    this.phraseService.decryptPhrase(dato).subscribe(data => {
+      this.dataTableDecrypt.unshift(data);
     })
 
   }
 
-  getDataDelete(select:any){
+  getDataDelete(select: any) {
     this.gettingPhrase.code = select.code;
     this.gettingPhrase.phrase = select.phrase;
     this.gettingPhrase.key = select.key;
-    this.onDeletePhrase(this.gettingPhrase,this.gettingPhrase.code)
+    this.onDeletePhrase(this.gettingPhrase, this.gettingPhrase.code)
   }
 
-  onDeletePhrase(data:IPhrase, id:number){
+  onDeletePhrase(data: IPhrase, id: number) {
     this.phraseService.deletePhrase(id)
-      .subscribe(()=>{
+      .subscribe(() => {
         this.onGetAllPhrases();
       })
   }
